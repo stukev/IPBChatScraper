6ef75adcdf8a3e62705bebc3235025de5fbafc4f
stukev <stukevcode@gmail.com>
2021-10-03 23:20:56 +0200

    Updated readme, created first version of a working script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # IPBChatScraper
-Scraping tool to backup Invision Power Board Chat messages
+Scraping tool to backup Invision Power Board "Chatbox" messages. Useful for OSINT, quicker data lookup with grep or simply just to have a backup in case forum admins purge the chat history.
+
+Tested with [Chatbox+](https://invisioncommunity.com/files/file/7465-chatbox-free/). Should also work with [Chatbox FREE](https://invisioncommunity.com/files/file/7465-chatbox-free/) but it's currently untested.
+
+## Usage
+
+1. Navigate to the forum chat in your browser, ensure that you're logged in.
+2. Open the developer console (F12) and search the sourcecode for "csrfKey". Note down the value. It will look similar to this: bf531df7ef66daef50441d6a08a042fe
+3. Open the browser settings and navigate to cookies. Input the forum url and open its cookies.
+4. You are looking for "ips4_IPSSessionFront". Note down the value. It will look similar to this: 1f85799402412bf1b30a69b704df46de
+5. Run the scraper with "python3 ipbchatscraper.py -csrf=yourvalue -session=yourvalue"
+
+### Optional Parameters
+* **-room=number** Defines the chat room. Default is 1 and will be used when parameter isn't provided.
+* **-file=filename** Output filename. Default is chatlog.txt.
+* **-continue=number** Continue an aborted backup process from given message id. Will backup the entire chatbox if not given.
+
+### Known bugs
+Sometimes the Chat API will refer to a non-existing lastid, causing the script to stop. At other times it randomly claims that the CSRF token is wrong (even though it's not). In both instances, resuming the script with the -continue=number option fixes this problem. If you're not sure what the last number was, simply open your output file and check the last added entry. Then add that id to the parameter. I might fix this with an automated retry.
---
+++ ipbchatscraper.py
@@ -0,0 +1,84 @@
+import json
+import time
+
+import requests
+
+url = ''
+cookie = ''
+csrf = ''
+room = '1'
+
+# TODO: get values via argparse instead of hardcode
+# TODO: add -continue flag to give lastid from aborted backup process
+# TODO: Bug: Sometimes script fails in line 75 as the reply is empty, needs to be handled and the request retried
+# TODO: Bug: Sometimes the API claims that our CSRF token is invalid when it's not. Retrying seems to fix this 100% of the time.
+
+lastid = '99999999999999'  # will default to the latest id
+
+def check_length(string):
+    if not len(string) == 32:
+        exit('Exiting: Session and CSRF hashes should be 32 characters each.')
+
+
+def check_errors(data, lastid):
+    if data == 'Something went wrong. Please try again.':
+        exit('Exiting: Your CSRF key is invalid or it has expired.')
+    if data[0:11] == '{"redirect"':
+        exit('Exiting: Your session key is invalid or it has expired.')
+    if data == '{"cacheLevel":"0","content":"","lastID":"","noOlder":"1"}':
+        exit('Exiting: Server said there are no more messages. Stopping at id ' + lastid + '.')
+
+
+def get_message(url, cookie, csrf, lastid='99999999999999', room='1'):
+    headers = {
+        'x-requested-with': 'XMLHttpRequest',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'cookie': 'ips4_IPSSessionFront=' + cookie + ';'
+    }
+
+    params = (
+        ('app', 'chatbox'),
+        ('module', 'chatbox'),
+        ('controller', 'room'),
+        ('id', room),
+        ('joined', room),
+        ('do', 'getMSG'),
+    )
+
+    data = {
+        'csrfKey': csrf,
+        'lastID': lastid,
+        'firstLoad': '0',
+        'loadMoreMode': '1',
+        'isReconnect': '0'
+    }
+    r = requests.post(url, headers=headers, params=params, data=data)
+    # check if there are any errors
+    check_errors(r.text, lastid)
+    return r.text
+
+
+def save_message(message, file='chatlog.txt'):
+    with open(file, "a") as output:
+        output.write(json.dumps(message) + '\n')
+
+
+print('Checking user input...')
+# check user input
+check_length(cookie)
+check_length(csrf)
+print('Looks good! Retrieving messages...')
+
+while 1:
+    # retrieve messages
+    message_json = get_message(url, cookie, csrf, lastid)
+    # convert to dict
+    message_dict = json.loads(message_json)
+    # loop through all messages
+    for i in message_dict['content']:
+        save_message(i)
+        lastid = i['id']
+    # sleep a bit to be nice to the server
+    time.sleep(2)
+    # Show progress
+    print('Progress: ' + lastid + ' more messages to backup...')
